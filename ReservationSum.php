e8e5e2c5233771b72b299570c8093c609b56334e
lin2772 <lin.2772@osu.edu>
2019-04-11 12:05:57 -0400

    sum up function in admin page is done

---
+++ ReservationSum.php
@@ -0,0 +1,124 @@
+<?php
+  $START=$_POST['startdate'];
+  $END=$_POST['enddate'];
+  $EVENT = $_POST['event'];
+  $con = mysqli_connect('localhost','phpuser','phpwd','parkingmaster');
+  If (!$con)
+  {
+    die('Could not connect'.mysqli_connect_error());
+  }
+  if(empty($EVENT)){
+    $sql_a = "  select garagename,total_reservation,total_space from
+    (select * from 
+        (select garage,  SUM(reserve) as total_reservation from 
+        (select reservation.garageid AS garage, reservation.levelnum AS level, count(reservationid) As reserve
+         from reservation join garage_level_event_date 
+         where reservation.garageid=garage_level_event_date.garageid
+          and reservation.levelnum=garage_level_event_date.levelnum
+          and dateCancelled is null
+          and reservation.eventid=garage_level_event_date.eventid
+          and reservation.date=garage_level_event_date.date
+          and dateReserved<='$END'
+          and dateReserved>='$START'
+          GROUP BY garage, level) AS layer1
+          Group BY garage) as a1
+          right join
+          (select garageid, SUM(maxspaces) As total_space
+          from garage_level
+          group by garageid) as a2
+          on a1.garage = a2.garageid) As t1
+          join 
+          garage as t2 
+          on t1.garageid = t2.garageid";
+  }
+  else{
+    $sql_a= " select garagename,t1.garageid, total_reservation, total_revenue, total_space from
+    (select * from 
+        (select garage, SUM(revenue) AS total_revenue, SUM(reserve) as total_reservation from 
+        (select reservation.garageid AS garage, reservation.levelnum AS level, count(reservationid) As reserve, count(reservationid)*price AS revenue 
+         from reservation join garage_level_event_date
+         where reservation.garageid=garage_level_event_date.garageid
+          and reservation.levelnum=garage_level_event_date.levelnum
+          and dateCancelled is null
+          and reservation.eventid=garage_level_event_date.eventid
+          and reservation.eventid = '$EVENT'
+          and reservation.date=garage_level_event_date.date
+          and dateReserved<='$END'
+          and dateReserved>='$START'
+          GROUP BY garage, level) AS layer1
+          Group BY garage) as a1
+          right join
+          (select garageid, SUM(maxspaces) As total_space
+          from garage_level
+          group by garageid) as a2
+          on a1.garage = a2.garageid) as t1
+          join garage as t2
+          on t1.garageid=t2.garageid";
+  }
+  $result = mysqli_query($con, $sql_a) or die(mysql_error($con));
+?>
+
+<html>
+<head>
+<title>Reservation sum up</title>
+</head>
+<body>
+    <h2>Reservation Sum up</h2>
+    <h3>Time Period <?php echo $START, " to ", $END?></h3>
+    <div>   
+      <table border="1px" >
+        <?php
+          if(empty($EVENT)){
+            echo "<tr><th>Garage</th> <th>Total reservation</th><th>Max capacity</th></tr>";
+            if(mysqli_num_rows($result)>0){
+              while ($row=mysqli_fetch_array($result)) {
+                $revervation = is_null($row['total_reservation'])? 0:$row['total_reservation'];
+                echo "<tr><td>{$row['garagename']}</td><td>$revervation</td><td>{$row['total_space']}</td></tr>";
+              }
+            }
+          }
+          else{
+             echo "<tr><th>Garage</th> <th>Total reservation</th><th>Max capacity</th><th>Total revenue</th></tr>";
+             if(mysqli_num_rows($result)>0){
+                while ($row=mysqli_fetch_array($result)) {
+                  $revervation = is_null($row['total_reservation'])? 0:$row['total_reservation'];
+                  $revenue = is_null($row['total_revenue'])? 0:$row['total_revenue'];
+                  echo "<tr><td>{$row['garagename']}</td><td>$revervation</td><td>{$row['total_space']}</td><td>$revenue</td></tr>";
+                }
+              }
+
+          }
+        ?>
+      </table>
+    </div>
+</body>
+</html>
+
+<!--
+select garagename,t1.garageid, total_reservation, total_revenue, total_space from
+    (select * from 
+        (select garage, SUM(revenue) AS total_revenue, SUM(reserve) as total_reservation from 
+        (select reservation.garageid AS garage, reservation.levelnum AS level, count(reservationid) As reserve, count(reservationid)*price AS revenue 
+         from reservation join garage_level_event_date join event
+         where reservation.garageid=garage_level_event_date.garageid
+          and reservation.levelnum=garage_level_event_date.levelnum
+          and dateCancelled is null
+          and reservation.eventid=garage_level_event_date.eventid
+          and event.eventname ='FinalExam'
+          and event.eventid = reservation.eventid
+          and reservation.date=garage_level_event_date.date
+          and dateReserved<='2019-03-09'
+          and dateReserved>='2019-03-06'
+          GROUP BY garage, level) AS layer1
+          Group BY garage) as a1
+          right join
+          (select garageid, SUM(maxspaces) As total_space
+          from garage_level
+          group by garageid) as a2
+          on a1.garage = a2.garageid) as t1
+          join garage as t2
+          on t1.garageid=t2.garageid
+
+
+
+          -->
--- adminPage.php
+++ adminPage.php
@@ -9,6 +9,14 @@
   //  echo $username;
   //echo $firstname;
   echo "Welcome, ",$firstname;
+  $con = mysqli_connect('localhost','phpuser','phpwd','parkingmaster');
+  If (!$con)
+  {
+    die('Could not connect'.mysqli_connect_error());
+  }
+  $sql_event="select * from event";
+  $all_event=array();
+  $result_event= mysqli_query($con, $sql_event) or die(mysqli_error($con));
 
 
  ?>
@@ -17,8 +25,61 @@
 <title>Welcome to Your Homepage admin</title>
 </head>
 <body>
-  <p>
-  need to be fixed
-  </p>
+  <div>
+    <table id="event_bar">
+      <tr>
+        <td>
+          <form action="" method="post">
+            <input type="submit" value="create new event">
+          </form>
+        </td>
+        <td>
+          <form action="" method="post">
+            <input type="submit" value="current event">
+          </form>
+        </td>
+      </tr>
+    </table>
+    <br><br>
+  </div>
+  <div>
+    <table id= "garage_bar">
+      <tr>
+        <td>
+          <form action="" method="post">
+            <input type="submit" value="edit_space">
+          </form>
+        </td>
+      </tr>
+    </table>
+    <br><br>
+  </div>
+  <div >
+    <table id="sumup_bar"></table>
+    <tr>
+      <th>Reservation Sum Up</th>
+      <th>    
+        <form action="ReservationSum.php" method="post">
+            <input type="date" name="startdate">
+            <input type="date" name="enddate">
+            <select name="event">
+              <option></option>
+              <?php
+                  if(mysqli_num_rows($result_event)>0){
+   
+                      while ($row=mysqli_fetch_array($result_event)) {
+                            echo "<option value={$row['eventid']}>{$row['eventname']}</option>";
+                           }
+                    }
+                ?>
+            </select>
+            <input type="submit" value="Submit">
+        </form>
+      </th>
+    </tr>
+   
+  </div>
+
+
 </body>
 </html>
